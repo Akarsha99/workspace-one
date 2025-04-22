ad0ef4aee12100c893c6711bfcfcdded0df5848b
Akarsha99 <mishraprowess.akarsha@gmail.com>
2025-04-22 14:16:12 +0530

    Add files via upload

--- Success.ps1
+++ Success.ps1
@@ -1,4 +1,8 @@
-﻿$result = $null
+﻿
+
+Del "C:\script\PendingReboot-Afterwin11Upgrade\User-PendingReboot-afterwin11Upgrade.ps1" /Q
+copy "$PSScriptRoot\User-PendingReboot-afterwin11Upgrade.ps1" "C:\script\PendingReboot-Afterwin11Upgrade"
+$result = $null
 
     $result = schtasks /query |findstr "PendingReboot-Afterwin11Upgrade"
     if($result)
